52fccc598b431f86d99231302556650949a29b08
kajedot <kaktu$007>
2021-11-07 18:27:44 +0100

    revert to 'port closed' + socket class

---
+++ fixes_communication/fixes_communication.py
@@ -0,0 +1,20 @@
+import socket
+
+
+class FixesCommunication:
+
+    def __init__(self, port):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind(('', port))
+            server_socket.listen()
+            self.conn, addr = server_socket.accept()
+            with self.conn:
+                print('Connected by', addr)
+
+    def send_data(self, data):
+        try:
+            self.conn.send(data)  # sending via lan socket
+
+        except (ValueError, IOError) as err:
+            print(err)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 from nmea_parser.nmea_parser import NmeaParser
+from fixes_communication.fixes_communication import FixesCommunication
 
 
 def main():
 
     parser = NmeaParser()
+    fixes_comm = FixesCommunication(65432)
 
     while 1:
         print("Fix mode: " + str( parser.get_fix_mode() ))
         print(parser.get_position())
+
+        fixes_comm.send_data(parser.listen())
 
 
 if __name__ == '__main__':
