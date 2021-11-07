cf67f660dd9d96c6f882d105ed89919a51fc6bd1
kajedot <kajetan.dt@gmail.com>
2021-11-07 19:19:42 +0100

    added client method

--- fixes_communication/fixes_communication.py
+++ fixes_communication/fixes_communication.py
@@ -1,20 +1,24 @@
 import socket
 
 
-class FixesCommunication:
+class FixesCommunicationClient:
 
-    def __init__(self, port):
+    def __init__(self, address, port):
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            server_socket.bind(('', port))
-            server_socket.listen()
-            self.conn, addr = server_socket.accept()
-            with self.conn:
-                print('Connected by', addr)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.client_socket:
+            self.client_socket.connect((address, port))
+            self.conn, addr = self.client_socket.accept()
 
     def send_data(self, data):
         try:
             self.conn.send(data)  # sending via lan socket
 
+        except (ValueError, IOError) as err:
+            print(err)
+
+    def receive_data(self):
+        try:
+            data = self.client_socket.recv(1024)
+            return data
         except (ValueError, IOError) as err:
             print(err)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 
 from nmea_parser.nmea_parser import NmeaParser
-from fixes_communication.fixes_communication import FixesCommunication
+from fixes_communication.fixes_communication import FixesCommunicationClient
 
 
 def main():
 
-    parser = NmeaParser()
-    fixes_comm = FixesCommunication(65432)
+    #parser = NmeaParser()
+    fixes_comm = FixesCommunicationClient("192.168.1.76", 65432)
 
     while 1:
-        print("Fix mode: " + str( parser.get_fix_mode() ))
-        print(parser.get_position())
+        #print("Fix mode: " + str( parser.get_fix_mode() ))
+        #print(parser.get_position())
 
-        fixes_comm.send_data(parser.listen())
+        data = fixes_comm.receive_data()
+        print(data)
 
 
 if __name__ == '__main__':
