6c180bf2fd473637c76093f43792d0e75adb6d4c
kajedot <kajedot@gmail.com>
2021-11-09 00:02:46 +0100

    with time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 
 from nmea_parser.nmea_parser import NmeaParser
 from fixes_communication.fixes_communication import FixesCommunication
@@ -14,6 +15,7 @@ def main():
 
         fixes_comm.send_data(b'eoo')
 
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
