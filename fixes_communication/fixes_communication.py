81854bd571a8ba54090ab31702b6d5fb6f04521a
kajedot <kajedot@gmail.com>
2021-11-08 22:48:23 +0100

    again outside int

--- fixes_communication/fixes_communication.py
+++ fixes_communication/fixes_communication.py
@@ -3,16 +3,15 @@ import socket
 
 class FixesCommunication:
 
-    def __init__(self):
+    def send_data(self, data):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             server_socket.bind(('', 65432))
             server_socket.listen()
             self.conn, self.addr = server_socket.accept()
 
-    def send_data(self, data):
-        with self.conn:
-            print('Connected by', self.addr)
-            try:
-                self.conn.send(data)
-            except (ValueError, IOError) as err:
-                print(err)
+            with self.conn:
+                print('Connected by', self.addr)
+                try:
+                    self.conn.send(data)
+                except (ValueError, IOError) as err:
+                    print(err)
