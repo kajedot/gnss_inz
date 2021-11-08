cbdd18e4679fa127206d96fe85b419b124447040
kajedot <kajedot@gmail.com>
2021-11-08 21:41:19 +0100

    comm from gitlab

--- fixes_communication/fixes_communication.py
+++ fixes_communication/fixes_communication.py
@@ -3,20 +3,13 @@ import socket
 
 class FixesCommunicationClient:
 
-    def send_data(self, data):
-        try:
-            self.conn.send(data)  # sending via lan socket
-
-        except (ValueError, IOError) as err:
-            print(err)
-
     def receive_data(self, address, port):
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.client_socket:
-            self.client_socket.connect((address, port))
-
-        try:
-            data = self.client_socket.recv(1024)
-            print(data)
-        except (ValueError, IOError) as err:
-            print(err)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as web_socket:
+            web_socket.connect((address, port))
+            while True:
+                try:
+                    data = web_socket.recv(1024)
+                    print(data)
+                except (ValueError, IOError) as err:
+                    print(err)
