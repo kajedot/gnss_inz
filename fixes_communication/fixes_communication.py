d4d43823bad19c9b01704dfc85f7562f62708993
kajedot <kajedot@gmail.com>
2021-11-16 21:49:08 +0100

    socket.listen(1)

--- fixes_communication/fixes_communication.py
+++ fixes_communication/fixes_communication.py
@@ -1,6 +1,4 @@
 import socket
-from threading import Thread
-from datetime import datetime
 
 
 class FixesCommunication:
@@ -11,7 +9,6 @@ class FixesCommunication:
         # put the private (network) IP address (e.g 192.168.1.2)
         SERVER_HOST = "192.168.1.103"
         SERVER_PORT = 5002  # server's port
-        self.separator_token = "<SEP>"  # we will use this to separate the client name & message
 
         # initialize TCP socket
         self.tcp_socket = socket.socket()
@@ -19,22 +16,6 @@ class FixesCommunication:
         # connect to the server
         self.tcp_socket.connect((SERVER_HOST, SERVER_PORT))
         print("[+] Connected.")
-        self.name = "kdt"
-
-        #self.start_thread()
-
-    def listen_for_messages(self):
-        while True:
-            message = self.tcp_socket.recv(1024).decode()
-            print("\n" + message)
-
-    def start_thread(self):
-        # make a thread that listens for messages to this client & print them
-        self.thread = Thread(target=self.listen_for_messages)
-        # make the thread daemon so it ends whenever the main thread ends
-        self.thread.daemon = True
-        # start the thread
-        self.thread.start()
 
     def client_loop(self, to_send):
         self.tcp_socket.send(to_send)
