f5d7bab79d5f84f63cca4f1a266bcfee127ab36a
kajedot <kajedot@gmail.com>
2021-11-16 18:17:21 +0100

    try send raw in main loop

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from fixes_communication.fixes_communication import FixesCommunication
 
 
 def main():
-    #parser = NmeaParser()
+    parser = NmeaParser()
     communication = FixesCommunication()
     while 1:
         #print("Fix mode: " + str( parser.get_fix_mode() ))
         #print(parser.get_raw())
 
-        communication.client_loop("eoeoeeo")
+        communication.client_loop(parser.get_raw())
 
 
 if __name__ == '__main__':
