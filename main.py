88e7be9542a55eaeb66942660b23be2657ceb61d
kajedot <kajedot@gmail.com>
2021-11-21 15:31:58 +0100

    test lines

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from ublox_communication.ublox_communication import UbloxCommunication
 
 def main():
     ublox_comm = UbloxCommunication()
-    #fixes_comm = FixesTransmissionServer(ublox_comm)
-
-    #fixes_comm.connections_listener()
 
     while 1:
         ublox_comm.check_fix_mode()
         #print("\nFix mode: " + str(ublox_comm.fix))
         #print(ublox_comm.get_position())
 
-        #ublox_comm.write(data)
-
-        print(ublox_comm.get_nmea_message("GNGGA"))
+        print(ublox_comm.lines_from_serial())
 
 
 if __name__ == '__main__':
