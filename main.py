db23c4924424fa35b3fef9d46c0d8c703580de19
kajedot <kajedot@gmail.com>
2021-12-14 19:27:01 +0100

    csv, nofixes

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def main():
     args = get_arguments()  # firstly manage command−line arguments
 
     ublox_comm = UbloxCommunication(args['device'], args['baudrate'])
-    fixes_trans = FixesTransmissionServer(ublox_comm, args['server'], args['port'], args['verbose'])
 
-    while 1:
-        print()
+    if args['nofixes']:
+        fixes_trans = FixesTransmissionServer(ublox_comm, args['server'], args['port'], args['verbose'])
+
+    while True:
         if args['verbose']:
             print(ublox_comm.get_nmea_message(b'GNGGA'))
 
@@ -30,8 +31,9 @@ def get_arguments():
     arg_parser.add_argument("-b", "--baudrate", required=False, default=38400, help="baud rate of transmission with the u-blox rover device (default: 38400)")
     arg_parser.add_argument("-s", "--server", required=False, default='0.0.0.0', help="ip address on which the server should run (default: 0.0.0.0)")
     arg_parser.add_argument("-p", "--port", required=False, default=5002, help="server's port (default: 5002)")
-    arg_parser.add_argument("-v", "--verbose", required=False, action='store_true', help="print received raw fixes data for the diagnostic purposes")  # store_true for arg. without value
+    arg_parser.add_argument("-n", "--nofixes", required=False, action='store_true', help="turn off fixes handling and WebSocket server") # store_true for arg. without value
     arg_parser.add_argument("-c", "--csv", required=False, action='store_true', help="print data in csv format: fix, latitude, latitude_dir, longitude, longitude_dir")
+    arg_parser.add_argument("-v", "--verbose", required=False, action='store_true', help="print received raw fixes data for the diagnostic purposes")
     return vars(arg_parser.parse_args())
 
 
