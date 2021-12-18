63ee93a582639401d83a6eea30147dd79128cb3b
kajedot <kajedot@gmail.com>
2021-12-18 13:46:44 +0100

    help

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def get_arguments():
     arg_parser.add_argument("-s", "--server", required=False, default='0.0.0.0', help="ip address on which the server should run (default: 0.0.0.0)")
     arg_parser.add_argument("-p", "--port", required=False, default=5002, help="server's port (default: 5002)")
     arg_parser.add_argument("-n", "--nofixes", required=False, action='store_true', help="turn off fixes handling and WebSocket server") # store_true for arg. without value
-    arg_parser.add_argument("-c", "--csv", required=False, action='store_true', help="print data in csv format: fix, latitude, latitude_dir, longitude, longitude_dir")
+    arg_parser.add_argument("-c", "--csv", required=False, action='store_true', help="print data in csv format: fix, latitude, latitude_dir, longitude, longitude_dir, GNGGA message")
     arg_parser.add_argument("-v", "--verbose", required=False, action='store_true', help="print received raw fixes data for the diagnostic purposes")
     return vars(arg_parser.parse_args())
 
